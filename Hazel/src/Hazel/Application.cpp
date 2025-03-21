d191536fe07dd2b10b04fcf4e68a6a1f4b83fa51
mathias2721 <95649013+mathias2721@users.noreply.github.com>
2025-03-21 13:42:37 +0100

    Moving to SandBox

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -14,7 +14,6 @@ namespace Hazel{
 	Application* Application::s_Instance = nullptr;
 
 	Application::Application()
-		:m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
 	{
 		HZ_CORE_ASSERT(!s_Instance, "Application already exist");
 		s_Instance = this;
@@ -25,118 +24,7 @@ namespace Hazel{
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
 
-		m_VertexArray.reset(VertexArray::Create());
 
-		float vertices[3 * 7] = {
-			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
-			 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-			 0.0f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-		};
-		std::shared_ptr<VertexBuffer> triangleVB;
-		triangleVB.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
-
-		BufferLayout layout =
-		{
-			{ShaderDataType::Float3, "a_Position"},
-			{ShaderDataType::Float4, "a_Color"},
-		};
-
-		triangleVB->SetLayout(layout);
-		m_VertexArray->AddVertexBuffer(triangleVB);
-
-		uint32_t indices[3] = { 0,1,2 };
-		std::shared_ptr<IndexBuffer> triangleIB;
-		triangleIB.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-		m_VertexArray->SetIndexBuffer(triangleIB);
-
-		m_SquareVA.reset(VertexArray::Create());
-
-		float squareVertices[4 * 3] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f,
-		};
-
-		std::shared_ptr<VertexBuffer> squareVB;
-		squareVB.reset(VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
-		squareVB->SetLayout({
-			{ShaderDataType::Float3, "a_Position"},
-			});
-		m_SquareVA->AddVertexBuffer(squareVB);
-
-		uint32_t squareIndices[6] = { 0,1,2, 2, 3, 0 };
-		std::shared_ptr<IndexBuffer> squareIB;
-		squareIB.reset(IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
-		m_SquareVA->SetIndexBuffer(squareIB);
-
-
-		std::string vertexSrc2 = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;
-			uniform mat4 u_ViewProjection;
-
-			out vec3 v_Position;
-
-			void main()
-			{
-				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
-			}
-		)";
-
-
-		std::string fragmentSrc2 = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-
-			void main()
-			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
-			}
-		)";
-
-		m_Shader2.reset(new Shader(vertexSrc2, fragmentSrc2));
-
-		std::string vertexSrc = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec4 a_Color;
-
-			uniform mat4 u_ViewProjection;
-			
-			out vec3 v_Position;
-			out vec4 v_Color;
-
-			void main()
-			{
-				v_Position = a_Position+1.0;
-				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
-			}
-		)";
-
-
-		std::string fragmentSrc = R"(
-			#version 330 core
-
-			in vec3 v_Position;
-			in vec4 v_Color;
-
-			layout(location = 0) out vec4 color;
-
-			void main()
-			{
-				color = v_Color;
-			}
-		)";
-
-		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
 	}
 
 	Application::~Application()
@@ -160,7 +48,6 @@ namespace Hazel{
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
-		dispatcher.Dispatch<KeyPressedEvent>(BIND_EVENT_FN(OnKeyArrow));
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
 		{
@@ -176,18 +63,6 @@ namespace Hazel{
 	{
 		while (m_Running)
 		{
-			RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
-			RenderCommand::Clear();
-
-			Renderer::BeginScene(m_Camera);
-
-			m_Camera.SetRotation({ 45.f});
-
-			Renderer::Submit(m_Shader2, m_SquareVA);
-
-			Renderer::Submit(m_Shader, m_VertexArray);
-			
-			Renderer::EndScene();
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
@@ -204,18 +79,6 @@ namespace Hazel{
 	bool Application::OnWindowClose(WindowCloseEvent& e)
 	{
 		m_Running = false;
-		return true;
-	}
-	
-	bool Application::OnKeyArrow(KeyPressedEvent& e)
-	{
-		switch (e.GetKeyCode())
-		{
-		case GLFW_KEY_LEFT:
-			m_Camera.Left();
-			return true;
-
-		}
 		return true;
 	}
 
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,20 +1,12 @@
 #pragma once
-
 #include "Core.h"
+
+#include "Window.h"
 #include "Hazel/LayerStack.h"
 #include "Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
 
 #include "Hazel/ImGui/ImGuiLayer.h"
-
-#include "Hazel/Renderer/Shader.h"
-#include "Hazel/Renderer/Buffer.h"
-#include "Hazel/Renderer/VertexArray.h"
-
-#include "Hazel/Renderer/OrthographicCamera.h"
-
-
-#include "Window.h"
 
 namespace Hazel{
 	class Application
@@ -39,14 +31,6 @@ namespace Hazel{
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
-
-		std::shared_ptr<Shader> m_Shader;
-		std::shared_ptr<VertexArray> m_VertexArray;
-
-		std::shared_ptr<Shader> m_Shader2;
-		std::shared_ptr<VertexArray> m_SquareVA;
-
-		OrthographicCamera m_Camera;
 
 		static Application* s_Instance;
 	};
