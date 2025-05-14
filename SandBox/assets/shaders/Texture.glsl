cb1683b7a09d809898123fed9e0fd80080e91a11
mathias2721 <95649013+mathias2721@users.noreply.github.com>
2025-05-14 12:58:23 +0200

    One shader for computation of color & texture

--- SandBox/assets/shaders/Texture.glsl
+++ SandBox/assets/shaders/Texture.glsl
@@ -22,9 +22,10 @@ layout(location = 0) out vec4 color;
 
 in vec2 v_TextCoord;
 
-uniform sampler2D u_Texture;			
+uniform sampler2D u_Texture;	
+uniform vec4 u_Color;			
 
 void main()
 {
-	color = texture(u_Texture, v_TextCoord * 10.0) * vec4(0.0, 0.0, 0.8, 1.0);
+	color = texture(u_Texture, v_TextCoord * 10.0) * u_Color;
 }
